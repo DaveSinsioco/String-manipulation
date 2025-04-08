9c0fd93fbaa6e0078ba7fb80a593aaea3d121a05
DaveSinsioco <sinciocodave@gmail.com>
2025-04-08 13:32:32 +0000

    printing name using lstrip to remove spaces from the left

--- name_related_string_manipulation/program_that_removes_spaces_from_the_beginning_when_user_input_their_full_name_with_spaces_at_the_beginning.py
+++ name_related_string_manipulation/program_that_removes_spaces_from_the_beginning_when_user_input_their_full_name_with_spaces_at_the_beginning.py
@@ -1,3 +1,7 @@
 # Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 
 name = str(input("input full name: "))
+
+# printing it using lstrip to remove spaces from the left
+
+print(name.lstrip())
