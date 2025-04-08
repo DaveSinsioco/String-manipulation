da7728a098c8a4d960ad35f08b6f56364db7ce74
DaveSinsioco <sinciocodave@gmail.com>
2025-04-08 14:17:58 +0000

    asks user to input their full name

--- name_related_string_manipulation/program_that_prints_the_input_in_all_caps_when_user_input_their_full_name.py
+++ name_related_string_manipulation/program_that_prints_the_input_in_all_caps_when_user_input_their_full_name.py
@@ -1,1 +1,4 @@
 # Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+# asks user to input their full name
+name = (input("Input your full name: "))
