6eec42e098d9f012a0685f9ecc54327e04d6676c
DaveSinsioco <sinciocodave@gmail.com>
2025-04-08 13:26:34 +0000

    asks them to input their name

---
+++ name_related_string_manipulation/program_that_removes_spaces_from_the_beginning_when_user_input_their_full_name_with_spaces_at_the_beginning.py
@@ -0,0 +1,3 @@
+# Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+
+name = str(input("input full name: "))
