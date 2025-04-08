027658dade796d80523cf33e34469d154ebdf2cd
DaveSinsioco <sinciocodave@gmail.com>
2025-04-08 13:43:28 +0000

    asks user to input number (0-1000)

--- name_related_string_manipulation/program_that_adds_zero_from_the beginning_to_complete_6_digit_number_when_user_input_a_number_(0-1000).py
+++ name_related_string_manipulation/program_that_adds_zero_from_the beginning_to_complete_6_digit_number_when_user_input_a_number_(0-1000).py
@@ -1,1 +1,5 @@
 # Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+
+
+# asks user to input number (0-1000)
+number = int(input("Input a number (0-1000): "))
