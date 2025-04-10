e25d833394d5eea8dd4de6bb76c03cc89dee613b
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 14:17:02 +0800

    ask the user to enter a string

---
+++ method_related_string_manipulation/program_that_imitates_capitalize_without_using_capitalize.py
@@ -0,0 +1,4 @@
+# capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+
+# ask the user to enter a string
+string = input("Enter a string: ")
