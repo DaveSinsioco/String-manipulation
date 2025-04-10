984166c76c4f50545fd929d27c39b2e38f3c1903
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 13:48:00 +0800

    check if the string ends with the suffix using rfind if they are similar then print outcome

--- method_related_string_manipulation/program_that_imitates_endswith_without_using_endswith.py
+++ method_related_string_manipulation/program_that_imitates_endswith_without_using_endswith.py
@@ -5,3 +5,9 @@ string = input("Enter a string: ")
 
 # ask user to enter a suffix
 suffix = input("Enter a suffix: ")
+
+# check if the string ends with the suffix
+if string.rfind(suffix) == len(string) - len(suffix):
+    print(f"The string '{string}' ends with the suffix '{suffix}'.")
+else:
+    print(f"The string '{string}' does not end with the suffix '{suffix}'.")    
