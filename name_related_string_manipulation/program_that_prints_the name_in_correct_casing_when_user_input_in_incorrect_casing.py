3dbfe7c8d5e6625f1397d6e8dc42904bfb7ae42f
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 11:20:01 +0800

    ask the user to input their name

---
+++ name_related_string_manipulation/program_that_prints_the name_in_correct_casing_when_user_input_in_incorrect_casing.py
@@ -0,0 +1,4 @@
+# Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+
+# ask the user to input their name 
+name = str(input("Enter your name: "))
