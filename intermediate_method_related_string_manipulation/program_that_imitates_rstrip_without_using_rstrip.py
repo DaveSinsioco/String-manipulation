69795f9fcfc87f37cfeeeece8c23ae09c2f758a4
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 14:51:29 +0800

    if the last character is a space, remove it

--- intermediate_method_related_string_manipulation/program_that_imitates_rstrip_without_using_rstrip.py
+++ intermediate_method_related_string_manipulation/program_that_imitates_rstrip_without_using_rstrip.py
@@ -3,5 +3,8 @@
 # ask user to enter a string
 string = input("Enter a string: ")
 
-# starts a while loop to check if the first character is a space
+# starts a while loop to check if the last character is a space
 while string.endswith(" "):
+
+    # if the last character is a space, remove it
+    string = string[:-1]
