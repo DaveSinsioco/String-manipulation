04d66b30edb4e8b0e3188c1440a168ade7491f3f
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 11:50:22 +0800

    prints the name in pascal case using title() and Replace() methods

--- name_related_string_manipulation/program_that_prints_the_name_in_pascal_case_when_user_input_their_full__name_in_incorrect_casing.py
+++ name_related_string_manipulation/program_that_prints_the_name_in_pascal_case_when_user_input_their_full__name_in_incorrect_casing.py
@@ -2,3 +2,6 @@
 
 # ask the user to input their name
 name = input("Enter your full name: ")
+
+# prints the name in pascal case using title() and Replace() methods
+print(name.title().replace(" ", ""))
