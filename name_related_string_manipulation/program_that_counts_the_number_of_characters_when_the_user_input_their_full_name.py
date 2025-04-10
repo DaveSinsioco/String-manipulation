2f4289c372b0cb82214cbdc91caeb1b3b10068a6
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 11:41:09 +0800

    ask the user to input their name

---
+++ name_related_string_manipulation/program_that_counts_the_number_of_characters_when_the_user_input_their_full_name.py
@@ -0,0 +1,4 @@
+# Create a program that ask the user to input their fullname. Print the number of characters in the input.
+
+# ask the user to input their name
+name = input("Enter your full name: ")
