0bdaf80b6c784fd4f6f470a0324f64f87a419a90
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 15:01:54 +0800

    ask the user to input the suffix to remove

---
+++ intermediate_method_related_string_manipulation/program_that_imitates_removesuffix_without_using_removesuffix.py
@@ -0,0 +1,4 @@
+# removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+
+# ask the user to input the suffix to remove
+suffix = input("Enter the suffix to remove: ")
