00a5b867a21b2b4f435eea3af1064024db7f7a5c
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 11:49:59 +0800

    ask the user to input their name

---
+++ name_related_string_manipulation/program_that_prints_the_name_in_pascal_case_when_user_input_their_full__name_in_incorrect_casing.py
@@ -0,0 +1,4 @@
+# Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+
+# ask the user to input their name
+name = input("Enter your full name: ")
