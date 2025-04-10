325ac8c7ba047060d5dfa489967212d184933cc8
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 15:07:39 +0800

    asks the user to enter a string

---
+++ intermediate_method_related_string_manipulation/program_that_imitates_upper_without_using_upper.py
@@ -0,0 +1,4 @@
+# upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+
+# asks the user to enter a string
+string = input("Enter a string: ")
