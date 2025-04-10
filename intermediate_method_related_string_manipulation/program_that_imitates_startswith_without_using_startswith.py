ff89a291da12623c4fca4c1215674270a49feeb7
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 15:20:56 +0800

    ask user to enter a string

---
+++ intermediate_method_related_string_manipulation/program_that_imitates_startswith_without_using_startswith.py
@@ -0,0 +1,4 @@
+# startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+
+# ask user to enter a string
+string = input("Enter a string: ")
