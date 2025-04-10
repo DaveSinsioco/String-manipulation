acf8dbf4a4ac3b06cd9020e989921a485ef96cad
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 14:30:14 +0800

    asks user to enter a string

---
+++ method_related_string_manipulation/program_that_imitates_title_without_using_title.py
@@ -0,0 +1,4 @@
+# title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+
+# asks user to enter a string
+string = input("Enter a string: ")
