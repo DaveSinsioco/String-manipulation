091c55bf56b4e78ab391020cb00ac33752e60d24
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 15:07:55 +0800

    prints the string in uppercase by making everything lowercase then swapcase

--- intermediate_method_related_string_manipulation/program_that_imitates_upper_without_using_upper.py
+++ intermediate_method_related_string_manipulation/program_that_imitates_upper_without_using_upper.py
@@ -2,3 +2,6 @@
 
 # asks the user to enter a string
 string = input("Enter a string: ")
+
+# prints the string in uppercase by making everything lowercase then swapcase
+print(string.lower().swapcase())
