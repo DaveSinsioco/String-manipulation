609949c6123b84027f0ce469411d813a7f165fb8
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 15:25:38 +0800

    asks the user to enter a string and length

---
+++ intermediate_method_related_string_manipulation/program_that_imitates_rjust_without_using_rjust.py
@@ -0,0 +1,5 @@
+# rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+
+# asks the user to enter a string and length
+string = input("Enter a string: ")
+length = int(input("Enter the length of the string needed: "))
