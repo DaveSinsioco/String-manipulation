0ac64c3cf62c2bdfc292caec370b1d7cb6c9ff14
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 12:59:55 +0800

    ask the user to input the prefix to remove

---
+++ method_related_string_manipulation/program_that_imitates_removeprefix_without_using_removeprefix.py
@@ -0,0 +1,4 @@
+# removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+
+# ask the user to input the prefix to remove
+prefix = input("Enter the prefix to remove: ")
