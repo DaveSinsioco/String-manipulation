f83d7f3863362cb7edf8dcd302cefc60982e2245
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 14:49:55 +0800

    ask user to enter a string

--- intermediate_method_related_string_manipulation/program_that_imitates_rstrip_without_using_rstrip.py
+++ intermediate_method_related_string_manipulation/program_that_imitates_rstrip_without_using_rstrip.py
@@ -1,1 +1,4 @@
+# rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
 
+# ask user to enter a string
+string = input("Enter a string: ")
