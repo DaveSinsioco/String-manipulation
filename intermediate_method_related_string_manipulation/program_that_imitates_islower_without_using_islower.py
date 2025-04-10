824694bd61505c2f9c60733cbfd78e7ea832dbbb
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 15:16:46 +0800

    ask user to enter a string

---
+++ intermediate_method_related_string_manipulation/program_that_imitates_islower_without_using_islower.py
@@ -0,0 +1,4 @@
+# islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+
+# ask user to enter a string
+string = input("Enter a string: ")
