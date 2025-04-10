91b29c897a03b139cd161368f5a5d0788c3eaa3e
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 16:01:08 +0800

    find the placement of the string from the last character

--- intermediate_method_related_string_manipulation/program_that_imitates_rindex_without_using_rindex.py
+++ intermediate_method_related_string_manipulation/program_that_imitates_rindex_without_using_rindex.py
@@ -3,3 +3,6 @@
 # ask the user for a string and a character to search
 string = input("Enter a string: ")
 char = input("Enter a character to search: ")
+
+# find the placement of the string from the last character
+placement = (string.rfind(char))
