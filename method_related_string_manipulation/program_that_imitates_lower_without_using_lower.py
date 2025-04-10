41a8103cc8a13fbc3887465b9e5ce7451ffeb858
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 13:08:27 +0800

    ask user to enter a string

---
+++ method_related_string_manipulation/program_that_imitates_lower_without_using_lower.py
@@ -0,0 +1,4 @@
+# lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+# ask user to enter a string
+string = input("Enter a string: ")
