26246f15b266f8fff81acd09add1b5c0f81eee9b
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 15:42:31 +0800

    ask the user for a string and a character to search

---
+++ intermediate_method_related_string_manipulation/program_that_imitates_index_without_using_index.py
@@ -0,0 +1,5 @@
+# index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+
+# ask the user for a string and a character to search
+string = input("Enter a string: ")
+char = input("Enter a character to search: ")
