587ad616695ba56955a18382b74d8aae996b891c
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 15:22:01 +0800

    print result after checking if the string start with the prefix

--- intermediate_method_related_string_manipulation/program_that_imitates_startswith_without_using_startswith.py
+++ intermediate_method_related_string_manipulation/program_that_imitates_startswith_without_using_startswith.py
@@ -5,3 +5,9 @@ string = input("Enter a string: ")
 
 # ask user to enter a prefix
 prefix = input("Enter a prefix: ")
+
+# check if the string start with the prefix
+if string.lfind(prefix) == 0:
+    print(f"The string '{string}' start with the prefix '{prefix}'.")
+else:
+    print(f"The string '{string}' does not start with the prefix '{prefix}'.")    
