ea8a5df08347a4e90346f23a61989d85fd521b73
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 13:23:20 +0800

    ask user to enter a string

---
+++ method_related_string_manipulation/program_that_imitates_isupper_without_using_isupper.py
@@ -0,0 +1,5 @@
+# isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+
+# ask user to enter a string
+string = input("Enter a string: ")
+
