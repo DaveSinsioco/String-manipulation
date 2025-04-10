a44c52f5c0edd04686d750ddab7b6697164d7151
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 14:02:08 +0800

    asks the user to enter a string and length

---
+++ method_related_string_manipulation/program_that_imitates_center_without_using_center.py
@@ -0,0 +1,5 @@
+# center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+
+# asks the user to enter a string and length
+string = input("Enter a string: ")
+length = int(input("Enter the length of the string needed: "))
