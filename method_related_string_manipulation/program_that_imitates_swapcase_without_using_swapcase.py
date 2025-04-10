87f60d4c57968c2ccbafff5df1b8e806fa7fbc8f
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 14:06:40 +0800

    ask the user to enter a string

---
+++ method_related_string_manipulation/program_that_imitates_swapcase_without_using_swapcase.py
@@ -0,0 +1,4 @@
+# swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+
+# ask the user to enter a string
+string = input("Enter a string: ")
