f31296bb54dbca27c82137172a551117edf10c18
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 13:53:21 +0800

    asks the user to enter a string and length

---
+++ method_related_string_manipulation/program_that_imitates_ljust_without_using_ljust.py
@@ -0,0 +1,6 @@
+# ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+
+# asks the user to enter a string and length
+string = input("Enter a string: ")
+length = int(input("Enter the length of the string needed: "))
+
