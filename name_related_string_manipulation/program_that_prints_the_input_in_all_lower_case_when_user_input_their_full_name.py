f57daf6f23b95549a1fd5980c2ee5a0cd6bc68ca
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 11:14:12 +0800

    print the name in all lower case using lower() method

---
+++ name_related_string_manipulation/program_that_prints_the_input_in_all_lower_case_when_user_input_their_full_name.py
@@ -0,0 +1,7 @@
+# Create a program that ask the user to input their fullname. Print the input in all lower case.
+
+# ask the user to input their name
+name = str(input("Enter your full name: "))
+
+# print the name in all lower case using lower() method
+print(name.lower())
