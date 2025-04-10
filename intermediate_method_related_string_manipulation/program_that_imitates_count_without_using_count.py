1e9f4f5251b1ef112a6a98e02b6d146656c28bf8
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 15:39:48 +0800

    asks for a string and subtext to count

---
+++ intermediate_method_related_string_manipulation/program_that_imitates_count_without_using_count.py
@@ -0,0 +1,7 @@
+# count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+
+# asks the user for a string
+string = input("Enter a string: ")
+
+# asks the user for a subtext to count
+char = input("Enter a subtext to count: ")
