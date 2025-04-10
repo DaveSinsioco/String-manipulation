7059e3c607203cb3610bf6c2ea19797368eb4c3b
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 11:54:13 +0800

    prints the name in pascal case using Lower() and Replace() methods

--- name_related_string_manipulation/program_that_prints__the_name_in_snake_case__when_user_input_their_full_name_in_incorrect_casing.py
+++ name_related_string_manipulation/program_that_prints__the_name_in_snake_case__when_user_input_their_full_name_in_incorrect_casing.py
@@ -2,3 +2,6 @@
 
 # ask the user to input their name
 name = input("Enter your full name: ")
+
+# prints the name in pascal case using Lower() and Replace() methods
+print(name.lower().replace(" ", "_"))
