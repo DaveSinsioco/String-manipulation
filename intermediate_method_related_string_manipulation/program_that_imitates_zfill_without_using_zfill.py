5dc84b27cc4af5e9351308bbb6ad6390f2ba1449
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 15:31:26 +0800

    prints the zeros multiplied by the difference between length and len(string) and adds the string

--- intermediate_method_related_string_manipulation/program_that_imitates_zfill_without_using_zfill.py
+++ intermediate_method_related_string_manipulation/program_that_imitates_zfill_without_using_zfill.py
@@ -1,1 +1,8 @@
 # zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+
+# asks user to input string and desired length
+string = input("Enter a string: ")
+length = int(input("Enter the desired length: "))
+
+# prints the zeros multiplied by the difference between length and len(string) and adds the string
+print("0" * (length - len(string)) + string)
