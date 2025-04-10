f16aa463f0c21b0ead37023509bb6d54889bbc6e
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 11:37:03 +0800

    split the statement into words using space as a delimiter

---
+++ name_related_string_manipulation/program_that_counts_the_number_of_words_in_a_statement.py
@@ -0,0 +1,7 @@
+# Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
+# ask the user to input a complete statement
+statement = input("Enter a complete statement: ")
+
+# split the statement into words using space as a delimiter
+words = statement.split()
