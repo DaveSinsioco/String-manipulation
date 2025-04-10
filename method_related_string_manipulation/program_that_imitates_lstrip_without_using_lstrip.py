e8379a7fe0bd7f10c7e74fc7c742028b996ea13f
DaveSinsioco <sinciocodave@gmail.com>
2025-04-10 12:51:13 +0800

    ask user to enter a string

--- method_related_string_manipulation/program_that_imitates_lstrip_without_using_lstrip.py
+++ method_related_string_manipulation/program_that_imitates_lstrip_without_using_lstrip.py
@@ -1,1 +1,5 @@
+# lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+
+# ask user to enter a string
+string = input("Enter a string: ")
 
